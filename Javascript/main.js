76b2a051e77803a4482b182e0e79199fd33c1091
HamoudyAwada <Mohamad.Awada04@outlook.com>
2024-11-17 15:43:22 -0700

    feat: create js file and add footer code
    I added inline JS to the about and contact page because that's the only JS code that I want to run on those pages.

---
+++ Javascript/main.js
@@ -0,0 +1,4 @@
+const footer = document.getElementById("footer");
+const x = new Date();
+footer.textContent = x.getFullYear();
+//to log the full date to the developer console, I performed: conosle.log(x.toDateString());, and that will print the full date to console
