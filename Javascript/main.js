c719584464a1d0bdb6f2f72be176ccc85fcb8b16
HamoudyAwada <Mohamad.Awada04@outlook.com>
2024-11-17 17:45:58 -0700

    style: add comments and line space

--- Javascript/main.js
+++ Javascript/main.js
@@ -3,6 +3,8 @@ const x = new Date();
 footer.textContent = x.getFullYear();
 //to log the full date to the developer console, I performed: conosle.log(x.toDateString());, and that will print the full date to console
 
+
+//creates a text that displays and returns a different message based on the time of day
 const hour = x.getHours();
 const greetingElement = document.getElementById("greeting");
 const greetingText = () => {
@@ -17,9 +19,12 @@ const greetingText = () => {
         return ("Good evening!");
     }
 };
+
+//makes an alert button that displays the value within the alert function
 greetingElement.textContent = greetingText();
 
 const clickme = document.getElementById('btn-alert')
 clickme.addEventListener('click', function(){
     alert('Welcome to my online portfolio!')
 })
+
