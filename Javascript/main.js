98b3d02a84187643bb3bed1800d64a01283c0c03
HamoudyAwada <Mohamad.Awada04@outlook.com>
2024-11-17 17:14:29 -0700

    feat: add time based greeting and linked styling to home page
    I added a small <p> tag with an associated ID to my home page for the content to display.

--- Javascript/main.js
+++ Javascript/main.js
@@ -2,3 +2,19 @@ const footer = document.getElementById("footer");
 const x = new Date();
 footer.textContent = x.getFullYear();
 //to log the full date to the developer console, I performed: conosle.log(x.toDateString());, and that will print the full date to console
+
+const hour = x.getHours();
+const greetingElement = document.getElementById("greeting");
+const greetingText = () => {
+    if (hour < 12) {
+        greetingElement.className = "morning";
+        return ("Good morning!");
+    } else if (hour < 17) {
+        greetingElement.className = "afternoon";
+        return ("Good, afternoon!");
+    } else {
+        greetingElement.className = "evening";
+        return ("Good evening!");
+    }
+};
+greetingElement.textContent = greetingText();
